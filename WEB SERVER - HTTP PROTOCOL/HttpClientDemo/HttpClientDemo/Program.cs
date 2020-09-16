557216788bace9cb360e9d27fb72ff2654dc6dfc
Kristina <kriska2000@abv.bg>
2020-09-16 21:04:36 +0300

    convert from byte array to text

--- WEB SERVER - HTTP PROTOCOL/HttpClientDemo/HttpClientDemo/Program.cs
+++ WEB SERVER - HTTP PROTOCOL/HttpClientDemo/HttpClientDemo/Program.cs
@@ -13,39 +13,22 @@ namespace HttpClientDemo
         static async Task Main(string[] args)
         {
             Console.OutputEncoding = Encoding.UTF8;
-            const string NewLine = "\r\n";  //only corect way to create new line
+            const string NewLine = "\r\n";  //only correct way to create new line
             TcpListener tcpListener = new TcpListener(IPAddress.Loopback, 80);
             tcpListener.Start();
 
             while (true)
             {
-                var client = tcpListener.AcceptTcpClient();
-                using (var stream = client.GetStream())
+                var client = tcpListener.AcceptTcpClient(); // read client
+                using (var stream = client.GetStream()) // take stream of client
                 {
-                    byte[] buffer = new byte[1000000];
+                    byte[] buffer = new byte[1000000]; // read all of stream
                     var lenght = stream.Read(buffer, 0, buffer.Length);
 
-                    string requestString =
-                        Encoding.UTF8.GetString(buffer, 0, lenght);
+                    string requestString = Encoding.UTF8.GetString(buffer, 0, lenght); // convert from byte array to text
                     Console.WriteLine(requestString);
 
-                    string html = $"<h1>Hello from NikiServer {DateTime.Now}</h1>" +
-                        $"<form action=/tweet method=post><input name=username /><input name=password />" +
-                        $"<input type=submit /></form>";
-
-                    string response = "HTTP/1.1 200 OK" + NewLine +
-                        "Server: NikiServer 2020" + NewLine +
-                        // "Location: https://www.google.com" + NewLine +
-                        "Content-Type: text/html; charset=utf-8" + NewLine +
-                        // "Content-Disposition: attachment; filename=niki.txt" + NewLine +
-                        "Content-Lenght: " + html.Length + NewLine +
-                        NewLine +
-                        html + NewLine;
-
-                    byte[] responseBytes = Encoding.UTF8.GetBytes(response);
-                    stream.Write(responseBytes);
-
-                    Console.WriteLine(new string('=', 70));
+                    
                 }
             }
         }
