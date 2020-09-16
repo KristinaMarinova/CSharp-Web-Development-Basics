db69a575321ce03f9f69a9fbd5f02012185fd367
Kristina <kriska2000@abv.bg>
2020-09-16 20:02:16 +0300

    reading info from address

--- WEB SERVER - HTTP PROTOCOL/HttpClientDemo/HttpClientDemo/Program.cs
+++ WEB SERVER - HTTP PROTOCOL/HttpClientDemo/HttpClientDemo/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace HttpClientDemo
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+        }
+
+        public async Task ReadData()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            string url = "https://softuni.bg/trainings/3164/csharp-web-basics-september-2020";
+            HttpClient httpClient = new HttpClient();
+            var html = await httpClient.GetStringAsync(url);
+            Console.WriteLine(html);
         }
     }
 }
