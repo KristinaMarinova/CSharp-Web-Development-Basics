04185f44351479981d3185692f5cf177af7ddb57
Kristina <kriska2000@abv.bg>
2020-09-28 19:41:27 +0300

    interface for cookies is added

---
+++ WEB SERVER - STATE MANAGEMENT/homework/IHttpCookieCollection.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+namespace homework
+{
+    interface IHttpCookieCollection : IEnumerable<HttpCookie>
+    {
+        void AddCookie(HttpCookie httpCookie);
+        bool ContainsCookie(string key);
+        HttpCookie GetCookie(string key);
+        bool HasCookies();
+    }
+}
