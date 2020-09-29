fb47f3f68b20bf5b33b37e4ff39c3e7a7851679d
Kristina <kriska2000@abv.bg>
2020-09-29 20:56:07 +0300

    wokshop

--- WORKSHOP/SUS/SUS.HTTP/HttpServer.cs
+++ WORKSHOP/SUS/SUS.HTTP/HttpServer.cs
@@ -1,17 +1,28 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
 
 namespace SUS.HTTP
 {
     public class HttpServer : IHttpServer
     {
+        IDictionary<string, Func<HttpRequest, HttpResponse>> routeTable = new Dictionary<string, Func<HttpRequest, HttpResponse>>();
         public void AddRoute(string path, Func<HttpRequest, HttpResponse> action)
         {
-            throw new NotImplementedException();
+            if (routeTable.ContainsKey(path))
+            {
+                routeTable[path] = action;
+            }
+            else
+            {
+                routeTable.Add(path, action);
+            }
         }
 
         public void Start(int port)
         {
-            throw new System.NotImplementedException();
+            TcpListener tcpListener = new TcpListener(IPAddress.Loopback, port);
         }
     }
 }
