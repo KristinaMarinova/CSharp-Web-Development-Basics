4fc738a044141c963066215a54324d4ba063388e
Kristina <kriska2000@abv.bg>
2020-10-16 00:14:09 +0300

    workshop

--- 04.WORKSHOP/SUS/SUS.HTTP/HttpResponce.cs
+++ 04.WORKSHOP/SUS/SUS.HTTP/HttpResponce.cs
@@ -1,20 +1,38 @@
 ﻿using System.Collections.Generic;
+using System.Text;
+
 namespace SUS.HTTP
 {
     public class HttpResponce
     {
-        public HttpResponce(HttpStatusCode statusCode, byte[] body)
+        public HttpResponce(string contentType, byte[] body, HttpStatusCode statusCode = HttpStatusCode.Ok)
         {
             this.StatusCode = statusCode;
             this.Body = body;
-            this.Header = new List<Header>
+            this.Headers = new List<Header>
             {
-                {new Header("Content-Type", "???") },
+                {new Header("Content-Type", contentType) },
                 {new Header("Content-Length", body.Length.ToString())},
             };
         }
+
+        public override string ToString()
+        {
+            StringBuilder responceBuilder = new StringBuilder();
+            responceBuilder.Append($"HTTP/1.1 {(int)this.StatusCode} {this.StatusCode}" + HttpConstants.NewLine);
+            foreach (var header in Headers)
+            {
+                responceBuilder.Append(header.ToString() + HttpConstants.NewLine);
+            }
+
+            responceBuilder.Append(HttpConstants.NewLine);
+
+            return responceBuilder.ToString();
+        }
+
+
         public HttpStatusCode StatusCode { get; set; }
-        public ICollection<Header> Header { get; set; }
+        public ICollection<Header> Headers { get; set; }
         public byte[] Body { get; set; }
     }
 }
