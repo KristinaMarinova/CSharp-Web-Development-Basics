015f0d653230e04879c42e28c71302a695829e17
Kristina <kriska2000@abv.bg>
2020-10-15 23:53:27 +0300

    workshop

--- 04.WORKSHOP/SUS/SUS.HTTP/HttpResponce.cs
+++ 04.WORKSHOP/SUS/SUS.HTTP/HttpResponce.cs
@@ -1,9 +1,10 @@
-﻿namespace SUS.HTTP
+﻿using System.Collections.Generic;
+
+namespace SUS.HTTP
 {
     public class HttpResponce
     {
-
-
+        public ICollection<Header> MyProperty { get; set; }
 
     }
 }
