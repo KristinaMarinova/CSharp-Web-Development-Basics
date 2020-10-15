d6147a8decf674551bf533fb0ebf5b6fbd28fb69
Kristina <kriska2000@abv.bg>
2020-10-16 00:02:00 +0300

    workshop

--- 04.WORKSHOP/SUS/SUS.HTTP/HttpResponce.cs
+++ 04.WORKSHOP/SUS/SUS.HTTP/HttpResponce.cs
@@ -3,9 +3,14 @@ namespace SUS.HTTP
 {
     public class HttpResponce
     {
+        public HttpResponce(HttpStatusCode statusCode, byte[] body)
+        {
+            this.StatusCode = statusCode;
+            this.Body = body;
+            this.Header = new List<Header>();
+        }
         public HttpStatusCode StatusCode { get; set; }
-        public ICollection<Header> MyProperty { get; set; }
-
+        public ICollection<Header> Header { get; set; }
         public byte[] Body { get; set; }
     }
 }
