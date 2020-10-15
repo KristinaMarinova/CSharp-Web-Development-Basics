633df686eeaa1f67a8d042ecec6c0d55b2485830
Kristina <kriska2000@abv.bg>
2020-10-15 23:55:23 +0300

    workshop

---
+++ 04.WORKSHOP/SUS/SUS.HTTP/HttpStatusCode.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SUS.HTTP
+{
+    public enum HttpStatusCode
+    {
+        Ok = 200,
+        NotFount = 404,
+        ServerError = 500
+    }
+}
