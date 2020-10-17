52dec0bb04bc643b8a4bc61215a810bc66aac68c
Kristina <kriska2000@abv.bg>
2020-10-17 12:47:29 +0300

    workshop

--- 04.WORKSHOP/SUS/SUS.HTTP/ResponseCookie.cs
+++ 04.WORKSHOP/SUS/SUS.HTTP/ResponseCookie.cs
@@ -1,4 +1,6 @@
-﻿namespace SUS.HTTP
+﻿using System.Text;
+
+namespace SUS.HTTP
 {
     public class ResponseCookie : Cookie
     {
@@ -9,5 +11,16 @@
         public int MaxAge { get; set; }
         public bool HttpOnly { get; set; }
         public string Path { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder cookieBuilder = new StringBuilder();
+            cookieBuilder.Append($"{this.Name} {this.Value}");
+
+            if (MaxAge != 0)
+            {
+                cookieBuilder.Append($" Max-Age={this.MaxAge}");
+            }
+        }
     }
 }
